05b083dbca5a6fc92cd870efcfe80fefa15db7be
Allan-Sanchez <asanchezrixtun@gmail.com>
2020-06-02 09:44:45 -0600

    fix form netlify

--- components/Contact.vue
+++ components/Contact.vue
@@ -6,22 +6,22 @@
 
                 <p class="para pl-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit dolores nemo ab aperiam repellendus sequi nostrum architecto sunt incidunt consequuntur?</p>
             </div>
-            <form action="POST" data-netlify="true">
+            <form name="contact" action="POST" data-netlify="true">
                 <div class="row">
                     <div class="col-md-6">
                         <h4 class="title-form">{{ $t('Contact.form1') }}</h4>
-                        <input type="text" class="form-control form-control-lg" placeholder="Allan Sanchez">
+                        <input type="text" name="name" class="form-control form-control-lg" placeholder="Allan Sanchez">
                     </div>
                     <div class="col-md-6">
                         <h4 class="title-form">{{ $t('Contact.form2') }}</h4>
-                        <input type="email" class="form-control form-control-lg" placeholder="emial@example.com">
+                        <input type="email" name="email" class="form-control form-control-lg" placeholder="emial@example.com">
                     </div>
 
                 </div>
                 <div class="row mt-3">
                     <div class="col-sm-12">
                         <h4 class="title-form">{{ $t('Contact.form3') }}</h4>
-                        <textarea class="form-control form-control-lg" id="exampleFormControlTextarea1" placeholder="Enter your message here" rows="6"></textarea>
+                        <textarea name="message" class="form-control form-control-lg" id="exampleFormControlTextarea1" placeholder="Enter your message here" rows="6"></textarea>
                     </div>
                     <div class="col-sm-12 col-md-7 col-lg-8 mt-5">
                         <div data-netlify-recaptcha="true"></div>
