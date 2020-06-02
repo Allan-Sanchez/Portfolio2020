e4865a89b5c98e7f5a5300bc9218bae649a80569
Allan-Sanchez <asanchezrixtun@gmail.com>
2020-06-02 11:24:16 -0600

    bot-fiel added

--- components/Contact.vue
+++ components/Contact.vue
@@ -7,26 +7,27 @@
                 <p class="para pl-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit dolores nemo ab aperiam repellendus sequi nostrum architecto sunt incidunt consequuntur?</p>
             </div>
 
-            <form name="contact-form" method="POST"  data-netlify="true">
+            <form name="contact-form"  action="/thanks" method="POST"  netlify netlify-honeypot="bot-field">
+                 <input type="hidden" name="form-name" value="contact-form" />
                 <div class="row">
                     <div class="col-md-6">
                         <h4 class="title-form">{{ $t('Contact.form1') }}</h4>
-                        <input type="text" name="name" class="form-control form-control-lg" placeholder="Allan Sanchez">
+                        <input type="text" name="name" class="form-control form-control-lg" placeholder="Allan Sanchez" required>
                     </div>
                     <div class="col-md-6">
                         <h4 class="title-form">{{ $t('Contact.form2') }}</h4>
-                        <input type="email" name="email" class="form-control form-control-lg" placeholder="emial@example.com">
+                        <input type="email" name="email" class="form-control form-control-lg" placeholder="emial@example.com" required>
                     </div>
 
                 </div>
                 <div class="row mt-3">
                     <div class="col-sm-12">
                         <h4 class="title-form">{{ $t('Contact.form3') }}</h4>
-                        <textarea name="message" class="form-control form-control-lg" id="exampleFormControlTextarea1" placeholder="Enter your message here" rows="6"></textarea>
+                        <textarea required name="message" class="form-control form-control-lg" id="exampleFormControlTextarea1" placeholder="Enter your message here" rows="6"></textarea>
                     </div>
-                    <div class="col-sm-12 col-md-7 col-lg-8 mt-5">
+                    <!-- <div class="col-sm-12 col-md-7 col-lg-8 mt-5">
                         <div data-netlify-recaptcha="true"></div>
-                    </div>
+                    </div> -->
                     <div class="col-sm-12 col-md-5 col-lg-4 mt-5">
                         <button type="submit" class="btn button primary-button mr-md-0 mr-lg-4 text-uppercase  float-right">{{ $t('Contact.btn')}}</button>
                     </div>
